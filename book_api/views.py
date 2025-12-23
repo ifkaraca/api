39042db370a4ad80ed5a8c80d867b77487fa3a97
prederyo <ifkaraca14@gmail.com>
2025-12-24 00:26:39 +0300

    Implement BookSerializer and update book_list view to use Django REST framework for response

--- book_api/views.py
+++ book_api/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from book_api.models import Book
+from book_api.serializer import BookSerializer
 
 # Create your views here.
+@api_view(['GET'])
 def book_list(request):
     books = Book.objects.all()
-    books_list = list(books.values())
-    return JsonResponse({
-        "books": books_list
-    })
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
 
 def create(request):
     pass
